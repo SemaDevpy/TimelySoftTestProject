af72b28f2cda0fff0fdca5d333172e3475f5c88a
SemaDevpy <syimykdev@gmail.com>
2021-02-16 12:32:19 +0600

    Initial Commit

---
+++ TimelySoftTestProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TimelySoftTestProject
+//
+//  Created by Syimyk on 2/16/21.
+//  Copyright © 2021 Syimyk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
